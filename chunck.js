58db882ef59ebccab1f0e7795b3dfa8eeb3d4367
mamyh <likhmaon@gmail.com>
2021-08-01 19:35:44 +0600

    so many new thing

--- chunck.js
+++ chunck.js
@@ -3,3 +3,32 @@ var b = a;
 a = 7;
 console.log(a);
 console.log(b);
+var arr = ['shakib', 'tamim'];
+var arr1 = arr;
+arr.push('mahmudullah');//change or mutate;
+arr = [];//assignment
+
+console.log(arr);
+console.log(arr1);
+
+//immutably=change na kore 
+var language = {
+    name: "javascript",
+    estd: 1995,
+    libraries: ['react', 'vue', 'jquery']
+}
+var language2 = _.cloneDeep(language)// { ...language };//one level reference copy
+language.founder = "Brender Erich";
+language.libraries.push('angular');
+console.log(language);
+console.log(language2)
+
+var sa = 'sakib';
+console.dir(sa);
+var sa1 = new String('sakib');
+console.log(sa1);
+
+// primitive values can not be mutable
+var mutable = 'sakib';
+mutable.test = 'tamim' //just ignore this line ,there will be no error
+console.log(mutable);
